94eb983195423317d0b1ea2d3baf4d2470d6be41
A^ <119428619+theatom06@users.noreply.github.com>
2025-10-20 10:33:40 +0000

    Added semver from uno.js

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
 # chai
 
-To install dependencies:
-
-```bash
-bun install
-```
-
-To run:
-
-```bash
-bun run index.ts
-```
-
-This project was created using `bun init` in bun v1.2.9. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+Lib/semver from [uno.js](https://github.com/theatom06/uno.js)
--- code/index.ts
+++ code/index.ts
@@ -1,6 +1,11 @@
 import { Command } from "commander";
-import details from "../package.json";
+import chaiJson from "../package.json";
+
 
 const chai = new Command();
 
-chai.
+chai
+    .name(chaiJson.name)
+    .description(chaiJson.description)
+    .version(chaiJson.version, "-v, --version", "output the current version")
+    .helpOption("-h, --help", "display help for command");
